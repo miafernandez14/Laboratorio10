10f7162f9ba7eb4665599ac72d50cf3c2d454316
luana <luana@GLADYS>
2023-06-20 13:19:10 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio10/Laboratorio10/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratorio10
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ViewToViewDemo();
+            //MainPage = new BindingModeDemo();
+            //MainPage = new ListViewDemo();
+            //MainPage = new PickerDemo();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
